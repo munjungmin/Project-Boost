8c41f719a1b02dabb7c9711570e87eb58366480c
munjungmin <answjdals77@gmail.com>
2023-01-19 23:20:01 +0900

    진동을 위한 Mathf.Sin()함수
    장애물이 자동으로 왔다 갔다 움직이게 하는 기능

--- Project Boost/Assets/Scripts/Oscillator.cs
+++ Project Boost/Assets/Scripts/Oscillator.cs
@@ -6,8 +6,8 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0, 1)] float movementFactor;
-
+    float movementFactor;
+    [SerializeField] float period = 2f;
     void Start()
     {
         startingPosition = transform.position;
@@ -17,6 +17,11 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period;  // continually growing over time
+        const float tau = Mathf.PI * 2;     // constant value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tau);   // 프레임을 반복하면서 -1 ~ 1 사이의 값이 반복됨   
+
+        movementFactor = (rawSinWave + 1f) / 2f;   // recalculated to go from 0 to 1 so its cleaner
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
